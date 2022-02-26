61819c8749ab0ac87f1a6f432410cfb12ebb622d
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-02-26 15:07:44 -0300

    Iniciando construção da layout do  projeto

---
+++ src/views/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as Styles from './styles'
+
+function Home(){
+    return(
+        <Styles.Container>
+            <Styles.Content>
+
+            </Styles.Content>
+        </Styles.Container>
+    )
+}
+
+export default Home
---
+++ src/views/Home/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Content = styled.div`
+    width: 600px;
+    height: 500px;
+    border-radius: 40px 0px 40px 0px;
+    background: #0F4571;
+
+
+border: 4px solid #386DBD;
+`
