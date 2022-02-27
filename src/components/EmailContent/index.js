875b9db2996a2a2fb3a9a56ab03555bd9332fc4a
Joallisson Nascimento Castro <69011012+Joallisson@users.noreply.github.com>
2022-02-26 22:59:44 -0300

    criando o botão de enviar

--- src/components/EmailContent/index.js
+++ src/components/EmailContent/index.js
@@ -3,26 +3,45 @@ import * as Styles from './styles'
 
 //ÍCONES
 import emailIcon from '../../Assets/emailIcon.png'
+import userIcon from '../../Assets/userIcon.png'
 
 function EmailContent(){
     return(
-        <Styles.Content>
+        <Styles.Container>
+            <Styles.Content>
 
-            <span id="faleConosco">Fale Conosco</span>
+                <span id="faleConosco">Fale Conosco</span>
 
-            <Styles.NameContent>
-                <label for="nameInput">NOME COMPLETO</label>
-                <input id="nameInput" type="text" placeholder="Digite o seu nome..."/>
-                <img src={emailIcon} alt="emailIcon"/>
-            </Styles.NameContent>
+                <Styles.NameContent>
+                    <label for="nameInput">NOME COMPLETO</label>
+                    <input id="nameInput" type="text" placeholder="Digite o seu nome..."/>
+                    <img src={userIcon} alt="emailIcon"/>
+                </Styles.NameContent>
 
-            <Styles.EmailContent>
-                <label for="emailInput">E-MAIL DE DESTINO</label>
-                <input id="emailInput" type="email" placeholder="Digite o e-mail de destino..."/>
-                <img src={emailIcon} alt="emailIcon"/>
-            </Styles.EmailContent>
+                <Styles.EmailContent>
+                    <label for="emailInput">E-MAIL DE DESTINO</label>
+                    <input id="emailInput" type="email" placeholder="Digite o e-mail de destino..."/>
+                    <img src={emailIcon} alt="emailIcon"/>
+                </Styles.EmailContent>
 
-        </Styles.Content>
+                <Styles.Subject>
+                    <Styles.TitleContent>
+                        <label for="inputTitle">TÍTULO DA MENSAGEM</label>
+                        <input placeholder="Informe o assunto principal" id="inputTitle" type="text"/>
+                    </Styles.TitleContent>
+
+                    <Styles.Description>
+                        <label for="inputDescription">TÍTULO DA MENSAGEM</label>
+                        <textarea placeholder="Do que se trata" id="inputDescription"/>
+                    </Styles.Description>
+                </Styles.Subject>
+
+                
+
+            </Styles.Content>
+
+            <button id="btnEnviar">Enviar</button>
+        </Styles.Container>
     )
         
 }
--- src/components/EmailContent/styles.js
+++ src/components/EmailContent/styles.js
@@ -1,8 +1,17 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+    button{
+        width: 161px;
+        height: 64px;
+        position: absolute;
+        bottom: 100px;
+    }
+`
+
 export const Content = styled.div`
-    width: 650px;
-    height: 550px;
+    width: 700px;
+    height: 600px;
     border-radius: 40px 0px 40px 0px;
     background: #0F4571;
     border: 4px solid #386DBD;
@@ -16,9 +25,10 @@ export const Content = styled.div`
     #faleConosco{
         font-weight: bold;
         font-size: 36px;
+        text-shadow: -5px 3px 4px rgba(1,1,1,0.3);
 
         position: absolute;
-        top: 30px;
+        top: 15px;
         color: #3A3132;
     }
 `
@@ -43,11 +53,11 @@ export const NameContent = styled.div`
     }
 
     img{
-        width: 25px;
+        width: 20px;
         height: 18px;
         position: relative;
         bottom: 25px;
-        left: 5px;
+        left: 8px;
     }
 `
 
@@ -75,5 +85,59 @@ export const EmailContent = styled.div`
         position: relative;
         bottom: 25px;
         left: 5px;
+    }
+`
+
+export const Subject = styled.div`
+    width: 630px;
+    height: 340px;
+    border-radius: 10px;
+    background: #009DDD;
+    margin-top: 45px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const TitleContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 25px;
+    
+    label{
+        font-weight: bold;
+        margin-bottom: 3px;
+    }
+
+    input{
+        height: 30px;
+        width: 530px;
+        border-radius: 10px;
+        border: none;
+        font-size: 16px;
+        padding-left: 10px;
+    }
+
+    
+`
+
+export const Description = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    label{
+        font-weight: bold;
+        margin-bottom: 3px;
+    }
+
+    textarea{
+        width: 530px;
+        height: 170px;
+        border-radius: 10px;
+        font-size: 16px;
+        padding-top: 10px;
+        padding-left: 10px;
     }
 `
